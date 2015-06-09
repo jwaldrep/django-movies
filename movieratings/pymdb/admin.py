6840c903d66cfce389ae4640fad727a0b7c8db18
John Waldrep <johnwaldrep@gmail.com>
2015-06-09 15:31:56 -0400

    Add admin list_displays

--- movieratings/pymdb/admin.py
+++ movieratings/pymdb/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Movie, Rater, Rating
 
+
+class RaterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'age', 'gender', 'occupation', 'zip_code']
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title','release_date', 'genre', 'imbdb_url']
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'rating', 'movie', 'user', 'time']
+
 # Register your models here.
-admin.site.register(Rating)
-admin.site.register(Rater)
-admin.site.register(Movie)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(Rater, RaterAdmin)
+admin.site.register(Movie, MovieAdmin)
