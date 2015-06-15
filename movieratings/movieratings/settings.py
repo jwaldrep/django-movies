9953cead691e4ddbedeaf7169c8009ca1dde83a7
John Waldrep <johnwaldrep@gmail.com>
2015-06-15 13:40:07 -0400

    Convert to postgres database

--- movieratings/movieratings/settings.py
+++ movieratings/movieratings/settings.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = 'movieratings.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pymdb',
+        'USER': 'pymdb',
+        'HOST': '127.0.0.1',
+        # default port is 5432
+    },
+
 }
 
 
